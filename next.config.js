2acf6c6c875f4ed98c442f0ec2c3cca67f2c8f9d
Husni Adil Makmur <husni.adil@gmail.com>
2020-12-25 22:22:22 +0700

    Implement asset-manifest.json generation

--- next.config.js
+++ next.config.js
@@ -1,10 +1,28 @@
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+const withPlugins = require('next-compose-plugins')
 const withPWA = require('next-pwa')
+const withAssetsManifest = require('next-assets-manifest')
 
 module.exports = (phase) =>
-  withPWA({
-    pwa: {
-      disable: phase === PHASE_DEVELOPMENT_SERVER,
-      dest: 'public',
-    },
-  })
+  withAssetsManifest(
+    withPWA({
+      pwa: {
+        disable: phase === PHASE_DEVELOPMENT_SERVER,
+        dest: 'public',
+      },
+      assetsManifest: {
+        customize(entry, original, manifest, asset) {
+          return {
+            key: entry.key,
+            value: '/_next/' + entry.value,
+          }
+        },
+        done(manifest) {
+          console.log(`${manifest}`)
+        },
+      },
+      assetsManifestClient: {
+        output: '../public/asset-manifest.json',
+      },
+    })
+  )
